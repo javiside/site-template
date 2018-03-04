ae7b0abddfb2dde810b272cf687c88fe11d0e28c
Javier <javi.mtz@hotmail.com>
2018-03-04 14:52:15 -0800

    [Bug fixed] Updated deprecated function (from jQuery 2.2.4 to v3.3.1)

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11363,7 +11363,7 @@
 	  _createClass(StickyHeader, [{
 	    key: 'refreshWaypoints',
 	    value: function refreshWaypoints() {
-	      this.lazyImages.load(function () {
+	      this.lazyImages.on('load', function () {
 	        Waypoint.refreshAll();
 	      });
 	    }
